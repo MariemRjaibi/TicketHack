4d5ea18faeeb7c7de1613a9f32dc6a48bbc97330
MariemRjaibi <78799426+MariemRjaibi@users.noreply.github.com>
2022-10-13 17:47:26 +0200

    Add files via upload

---
+++ backend/models/bookings.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = mongoose.Schema({
+  trip: { type: mongoose.Schema.Types.ObjectId, ref: 'trips' },
+  isPaid: Boolean,
+});
+
+const Booking = mongoose.model('bookings', bookingSchema);
+
+module.exports = Booking;
+  
